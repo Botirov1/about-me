2e5eef07266442f9da326f45fb4295870aa13ace
Botirov1 <139611915+Botirov1@users.noreply.github.com>
2023-10-19 02:14:56 +1400

    Add files via upload

---
+++ rezyume/main.js
@@ -0,0 +1,72 @@
+// // $(function(){
+
+// // $("#but1").click(function(){
+// // $(".navbar")
+// // $(".but1")
+// // $(".but2")
+// // $(".but3")
+// // $(".but4")
+// // let xozir = "xoz";
+// // if (xozir = ) {
+    
+
+
+// // }
+
+
+
+
+// //     // $(".navbar").slideToggle(500);     
+    
+// //     //  pastdan tepaga yoq bolib ketadi
+
+
+
+
+
+
+// // })
+
+ 
+// // })
+
+
+
+// // function ishla() {
+// //  let xozir = "xoz"   
+// // let navbar = document.querySelector(".navbar")
+// // let but1 = document.querySelector("but1")
+// // let but2 = document.querySelector("but2")
+// // let but3 = document.querySelector("but3")
+// // let but4 = document.querySelector("but4")
+// // let h3 = document.querySelector(".navbar h3")
+// // let h5 = document.querySelector(".navbar h5")
+// // if (xozir == "xoz") {
+    
+// // navbar.style.marginLeft = "200px"
+// // h3.style.color = "white"
+// // h3.style.marginLeft = "-240px"
+// // but1.style.marginTop = "200px"
+// // h5.style.color = "white"
+// // // h5.style.marginLeft = "-240px"
+// // h3.style.marginTop = "20px"
+// // xozir = "xozir"
+// // }
+// // else
+// // {
+// // navbar.style.marginRight = "400px"
+// // xozir = "xoz"
+// // }
+
+// // }
+
+
+// $(".fa-brands fa-space-awesome").click(function(){
+
+//    $(".fa-brands fa-space-awesome").css("margin-bottom", "850px")
+// alert("dbhc")
+
+
+
+
+// })
